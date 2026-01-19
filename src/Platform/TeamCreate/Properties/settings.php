44ead5de7c09a19017f090129bb0b15ad5e4dedc
Harley <mathmark825@gmail.com>
2026-01-19 23:24:20 +1100

    Patch: Add TeamCreate properties

---
+++ src/Platform/TeamCreate/Properties/settings.php
@@ -0,0 +1,14 @@
+<?php
+/*
+*   Source: Roblox.Platform.TeamCreate.Properties
+*/
+
+return [
+    "Default" => [
+        "IsTeamCreatePermissionsV1WriteEnabled" => true,
+        "AwsTeamCreateEventTopicAccessKeyIdAndSecretCsv" => "",
+        "AwsTeamCreateEventSnsTopicArn" => "",
+        "IsAwsTeamCreateEventPublishingEnabled" => false,
+        "IsTeamCreateKickUserOnPermissionLossEnabled" => true
+    ],
+];
--- src/Providers/GooberBloxSettingsServiceProvider.php
+++ src/Providers/GooberBloxSettingsServiceProvider.php
@@ -12,11 +12,16 @@ class GooberBloxSettingsServiceProvider extends ServiceProvider
             __DIR__ . '/../AssetMedia/Properties/settings.php',
             'gooberblox.assetmedia'
         );
+        $this->mergeConfigFrom(
+            __DIR__ . '/../Platform/TeamCreate/Properties/settings.php',
+            'gooberblox.teamcreate'
+        );
     }
     public function boot(): void
     {
         $this->publishes([
             __DIR__ . '/../AssetMedia/Properties/settings.php' => config_path('gooberblox/assetmedia.php'),
+            __DIR__ . '/../Platform/TeamCreate/Properties/settings.php' => config_path('gooberblox/teamcreate.php'),
         ], 'gooberblox-config');
         
     }
