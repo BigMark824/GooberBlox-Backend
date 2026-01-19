947d14f3e133684bf93c6adcebfc05b5525eba25
Harley <mathmark825@gmail.com>
2026-01-19 23:00:54 +1100

    Patch: Finish AssetMedia properties

--- src/AssetMedia/Properties/settings.php
+++ src/AssetMedia/Properties/settings.php
@@ -1,8 +1,14 @@
 <?php
+/*
+*   Source: Roblox.AssetMedia.Properties
+*/
 
 return [
     "Default" => [
+        "UsePlaceMediaItemForThumb" => false,
         "MaximumPlaceMediaItemsPerPlace" => 100,
+        "AllowPlaceMediaUpload" => false,
         "MaximumPluginAssetMediaCount" => 1,
+        "GetAssetMediaItemsPageSize" => 100,
     ],
 ];
