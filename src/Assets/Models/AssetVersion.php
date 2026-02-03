ec0990b22fae6a9f3f8bef71ef05d9ddbccfc898
Harley <mathmark825@gmail.com>
2026-02-03 17:20:36 +1100

    Add AssetVersion migration & model

---
+++ src/Assets/Models/AssetVersion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GooberBlox\Assets\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AssetVersion extends Model
+{
+    protected $fillable = [
+        'asset_id',
+        'version_number',
+        'asset_hash_id',
+        'parent_asset_version_id',
+        'creator_type',
+        'creator_target_id',
+        'creating_universe_id'
+    ];
+}
---
+++ src/DataAccess/migrations/2026_02_03_061434_create_asset_versions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asset_versions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('asset_id');
+            $table->integer('version_number');
+            $table->bigInteger('asset_hash_id');
+            $table->bigInteger('parent_asset_version_id')->nullable();
+            $table->enum('creator_type', ['User','Group'])->default('User');
+            $table->bigInteger('creator_target_id')->comment('Creator agent ID');
+            $table->bigInteger('creating_universe_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asset_versions');
+    }
+};
