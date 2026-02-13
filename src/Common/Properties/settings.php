9a7f90f5c0f7585547e52aa47f8ef1218ce96871
Harley <mathmark825@gmail.com>
2026-02-13 16:07:39 +1100

    Feature: add GameServerRequestValidator & remove legacy GridChecker

--- src/Common/Properties/settings.php
+++ src/Common/Properties/settings.php
@@ -6,5 +6,8 @@
 return [
     "Default" => [
         "BetaFeaturePlaceIds" => "",
+        "VerifyBothGameServerIpAndAccessKey" => false, // ideally set this to true
+        "AccessKey" => env('GRID_ACCESS_KEY'),
+        "AlternateAccessKey" => env('GRID_ALTERNATE_ACCESS_KEY')
     ],
 ];
---
+++ src/Web/Code/GameServerRequestValidator.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace GooberBlox\Web\Code;
+
+use App\Http\Requests\InsensitiveRequest as Request;
+use GooberBlox\Platform\Infrastructure\Enums\ServerType;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+
+use GooberBlox\Platform\Infrastructure\Models\Server;
+class GameServerRequestValidator
+{
+    public Request $request;
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    public function isPotentialGameServer() : bool
+    {
+        return !empty($this->request->header('accessKey'));
+    }
+
+    public function isValidGameServerRequest() : bool
+    {
+        if ($this->request->attributes->has('ValidatedGameServerRequest')) {
+            return $this->request->attributes->get('ValidatedGameServerRequest');
+        }
+
+       $ipAddress = $this->request->ip();
+
+       $isValid = 
+            $this->verifyIpAddress($ipAddress) &&
+            $this->verifyAccessKey($ipAddress);
+
+        $this->request->attributes->set('ValidatedGameServerRequest', $isValid);
+
+        return $isValid;
+    }
+
+    private function verifyIpAddress(string $ipAddress) : bool
+    {
+        if(!config('gooberblox.common.Default.VerifyBothGameServerIpAndAccessKey') || $this->verifyIpAccess($ipAddress))
+        {
+            return true;
+        }
+
+        Log::warning('ip address not allowed', [$this->request->headers, $ipAddress]);
+        return false;
+    }
+
+    private function verifyAccessKey(string $ipAddress): bool
+    {
+        $accessKey = $this->request->header('accessKey');
+        if(empty($accessKey))
+        {
+            Log::warning('no access key', [$this->request->headers, $ipAddress]);
+            return false;
+        }
+
+        $isPrimaryAccessKey = $accessKey === config('gooberblox.common.Default.AccessKey');
+        $isAlternateAccessKey = $accessKey === config('gooberblox.common.Default.AlternateAccessKey');
+
+        if(!$isPrimaryAccessKey && !$isAlternateAccessKey)
+        {
+            Log::warning("invalid access key\n Access Key: {$accessKey}", [$this->request->headers, $ipAddress]);
+            return false;
+        }
+
+        if($isAlternateAccessKey && !$isPrimaryAccessKey)
+        {
+            Log::warning("alternate access key in use", [$this->request->headers, $ipAddress]);
+            return true;
+        }  
+
+        return true;
+    }
+    
+    // TODO: move this to base class ServerValidator
+    private function verifyIpAccess(string $ipAddress) : bool
+    {
+        $server = Server::where('primary_ip_address', $ipAddress)
+                            ->where('server_type_id', ServerType::GameServer)
+                            ->first();
+        
+        if(!$server)
+            return false;
+
+        return true;
+    }
+ }
