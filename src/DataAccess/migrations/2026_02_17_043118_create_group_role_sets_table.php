73d37d6bf9fe0c708225ecb652ebf833fdedcb37
Harley <mathmark825@gmail.com>
2026-02-17 15:41:21 +1100

    Whoops add rank to the migration

--- src/DataAccess/migrations/2026_02_17_043118_create_group_role_sets_table.php
+++ src/DataAccess/migrations/2026_02_17_043118_create_group_role_sets_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('group_role_sets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
+            $table->unsignedTinyInteger('rank');
             $table->foreignId('group_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
