2bdccae11ab6e2c9319e171a74e9ee2238e62841
Harley <mathmark825@gmail.com>
2026-05-12 17:42:24 +1000

    Add feed scope

--- src/Library/Models/UserFeed.php
+++ src/Library/Models/UserFeed.php
@@ -15,8 +15,22 @@ class UserFeed extends Model
         'feed_id'
     ];
 
-    public function Feed()
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function feed()
     {
         return $this->belongsTo(Feed::class);
     }
+
+    public function scopeStatus($query)
+    {
+        return $query->whereHas('feed', function($q)
+        {
+            $q->where('action_type', FeedType::status()->id);
+        });
+    }
+
 }
